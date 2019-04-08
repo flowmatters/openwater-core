bd981a1a898f7165d76ab026bd153954d3c2cf54
Joel Rahman <joel@flowmatters.com.au>
2019-04-08 14:42:12 +1000

    Install minimal python environment

--- build/bootstrap-test-env.sh
+++ build/bootstrap-test-env.sh
@@ -4,4 +4,6 @@ set -e
 
 python3 -m venv .ow-test
 source .ow-test/bin/activate
-pip3 --version
+pip --version
+curl 'https://raw.githubusercontent.com/flowmatters/openwater/master/requirements.txt' > requirements.txt
+pip install -r requirements.txt
