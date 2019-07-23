f9c00aafeef8a6f0d5083c3b7c57887322f3a7ea
Joel Rahman <joel@flowmatters.com.au>
2019-07-23 13:15:31 +1000

    Explicitly install wheel

--- build/bootstrap-test-env.sh
+++ build/bootstrap-test-env.sh
@@ -5,6 +5,7 @@ set -e
 python3 -m venv .ow-test
 source .ow-test/bin/activate
 pip --version
+pip install wheel
 curl 'https://raw.githubusercontent.com/flowmatters/openwater/master/requirements.txt' > requirements.txt
 pip install -r requirements.txt
 pip install https://github.com/flowmatters/openwater/archive/master.zip
