9da7a00a0790ba89992a8b7ba07b3374b685bc93
Joel Rahman <joel@flowmatters.com.au>
2020-01-24 11:41:53 +1100

    Added scaling factor model

---
+++ models/conversion/scale.go
@@ -0,0 +1,40 @@
+package conversion
+
+import (
+	"github.com/flowmatters/openwater-core/data"
+)
+
+/*OW-SPEC
+ApplyScalingFactor:
+  inputs:
+		input:
+	states:
+	parameters:
+		scale: 'default=1'
+	outputs:
+		output:
+	implementation:
+		function: applyScaling
+		type: scalar
+		lang: go
+		outputs: params
+	init:
+		zero: true
+		lang: go
+	tags:
+		partition
+*/
+
+func applyScaling(input data.ND1Float64,
+	scale float64,
+	output data.ND1Float64) {
+
+	nDays := input.Len1()
+	idx := []int{0}
+
+	for i := 0; i < nDays; i++ {
+		idx[0] = i
+		incoming := input.Get(idx)
+		output.Set(idx, incoming*scale)
+	}
+}
