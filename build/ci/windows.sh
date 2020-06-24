8cb46a51391c831f07aca1c4434ef845e225a2a9
Joel Rahman <joel@flowmatters.com.au>
2020-06-24 17:30:08 +1000

    Tweak windows path setup

--- build/ci/windows.sh
+++ build/ci/windows.sh
@@ -3,8 +3,8 @@
 curl -o hdf5.zip https://www.flowmatters.com.au/ow/1.10.5.zip
 unzip hdf5.zip
 export HDF5_DIR=`pwd -W`/1.10.5
-echo export CGO_CFLAGS="-I$HDF5_DIR/include" > compilation_vars.txt
-echo export CGO_LDFLAGS="-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl"  >> compilation_vars.txt
+echo export CGO_CFLAGS=\"-I$HDF5_DIR/include\" > compilation_vars.txt
+echo export CGO_LDFLAGS=\"-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl\"  >> compilation_vars.txt
 echo '--- compilation_vars.txt ---'
 cat compilation_vars.txt
 
