b1e417edea9809e28d0b7fa654556cf349680a56
Joel Rahman <joel@flowmatters.com.au>
2020-06-25 16:10:23 +1000

    CI:Windows - Add installed Python 3 to path

--- build/ci/windows.sh
+++ build/ci/windows.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+choco install python --version=3.7.2
+
 curl -o hdf5.zip https://www.flowmatters.com.au/ow/1.10.5.zip
-unzip hdf5.zip
+unzip -q hdf5.zip
 export HDF5_DIR=`pwd -W`/1.10.5
 echo export CGO_CFLAGS=\"-I$HDF5_DIR/include\" > compilation_vars.txt
 echo export CGO_LDFLAGS=\"-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl\"  >> compilation_vars.txt
+echo export PATH=\"$PATH:C:/Python37/bin\" >> compilation_vars.txt
+
 echo '--- compilation_vars.txt ---'
+
 cat compilation_vars.txt
 
 source compilation_vars.txt
@@ -13,4 +18,5 @@ echo HDF5_DIR: $HDF5_DIR
 echo CGO_CFLAGS: $CGO_CFLAGS
 echo CGO_LDFLAGS: $CGO_LDFLAGS
 
-choco install python --version=3.7.2
+
+echo $PATH
