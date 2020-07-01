6d3f64b39cc476e4c9c9c215fc53096f1419b953
Joel Rahman <joel@flowmatters.com.au>
2020-07-01 15:04:52 +1000

    CI: - Dealling with venv differences

--- build/bootstrap-test-env.sh
+++ build/bootstrap-test-env.sh
@@ -5,7 +5,7 @@ set -e
 python3 -c "import sys; print(sys.path)"
 python3 -m venv .ow-test
 echo ow-test created
-source .ow-test/bin/activate
+source .ow-test/$VENV_DIR/activate
 echo ow-test activated
 pip --version
 pip install wheel
--- build/ci/windows.sh
+++ build/ci/windows.sh
@@ -9,6 +9,7 @@ export HDF5_DIR=`pwd -W`/1.10.5
 echo export CGO_CFLAGS=\"-I$HDF5_DIR/include\" > compilation_vars.txt
 echo export CGO_LDFLAGS=\"-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl\"  >> compilation_vars.txt
 echo export PATH=\"$PATH:/c/Python37:/c/Python37/Scripts\" >> compilation_vars.txt
+echo export VENV_DIR=Scripts >> compilation_vars.txt
 
 echo '--- compilation_vars.txt ---'
 
