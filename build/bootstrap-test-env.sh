8924cb040e145ef61d595837160e0cbd104df06d
Joel Rahman <joel@flowmatters.com.au>
2020-07-01 16:42:42 +1000

    CI: - Dealling with venv differences

--- build/bootstrap-test-env.sh
+++ build/bootstrap-test-env.sh
@@ -3,12 +3,12 @@
 set -e
 
 python3 -c "import sys; print(sys.path)"
-ls -lh
-python3 -m venv ow-test
-ls -lh
-echo ow-test created
-source ow-test/$VENV_DIR/activate
-echo ow-test activated
+# ls -lh
+# python3 -m venv ow-test
+# ls -lh
+# echo ow-test created
+# source ow-test/$VENV_DIR/activate
+# echo ow-test activated
 pip --version
 pip install wheel
 curl 'https://raw.githubusercontent.com/flowmatters/openwater/master/requirements.txt' > requirements.txt
