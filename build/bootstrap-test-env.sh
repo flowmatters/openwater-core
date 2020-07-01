facf845fe333444981fe9c2407c3776621fa8721
Joel Rahman <joel@flowmatters.com.au>
2020-07-01 14:51:14 +1000

    CI: - Diagnostics

--- build/bootstrap-test-env.sh
+++ build/bootstrap-test-env.sh
@@ -3,9 +3,10 @@
 set -e
 
 python3 -c "import sys; print(sys.path)"
-python3 -m venv -h
 python3 -m venv .ow-test
+echo ow-test created
 source .ow-test/bin/activate
+echo ow-test activated
 pip --version
 pip install wheel
 curl 'https://raw.githubusercontent.com/flowmatters/openwater/master/requirements.txt' > requirements.txt
