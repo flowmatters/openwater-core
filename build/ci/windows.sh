1c16aeb222a5f21f558260d8d200495df46f811c
Joel Rahman <joel@flowmatters.com.au>
2020-07-08 15:53:50 +1000

    CI:Windows - Add HDF5 bin dir to path

--- build/ci/windows.sh
+++ build/ci/windows.sh
@@ -6,9 +6,10 @@ cp /c/Python37/python.exe /c/Python37/python3.exe
 curl -o hdf5.zip https://www.flowmatters.com.au/ow/1.8.21.zip
 unzip -q hdf5.zip
 export HDF5_DIR=`pwd -W`/1.8.21
+export HDF5_DIR_POSIX=`pwd`/1.8.21
 echo export CGO_CFLAGS=\"-I$HDF5_DIR/include\" > compilation_vars.txt
 echo export CGO_LDFLAGS=\"-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl\"  >> compilation_vars.txt
-echo export PATH=\"/c/Python37:/c/Python37/Scripts:$HDF5_DIR/bin:$PATH\" >> compilation_vars.txt
+echo export PATH=\"/c/Python37:/c/Python37/Scripts:$HDF5_DIR_POSIX/bin:$PATH\" >> compilation_vars.txt
 echo export VENV_DIR=Scripts >> compilation_vars.txt
 
 echo '--- compilation_vars.txt ---'
