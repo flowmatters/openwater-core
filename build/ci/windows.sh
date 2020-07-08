9241a0eb5a96532de9e7a8a484efb35d796cc9fa
Joel Rahman <joel@flowmatters.com.au>
2020-07-08 11:53:52 +1000

    Roll back Windows to HDF5 1.8.21

--- build/ci/windows.sh
+++ build/ci/windows.sh
@@ -3,9 +3,9 @@
 choco install python --version=3.7.2
 cp /c/Python37/python.exe /c/Python37/python3.exe
 
-curl -o hdf5.zip https://www.flowmatters.com.au/ow/1.10.5.zip
+curl -o hdf5.zip https://www.flowmatters.com.au/ow/1.8.21.zip
 unzip -q hdf5.zip
-export HDF5_DIR=`pwd -W`/1.10.5
+export HDF5_DIR=`pwd -W`/1.8.21
 echo export CGO_CFLAGS=\"-I$HDF5_DIR/include\" > compilation_vars.txt
 echo export CGO_LDFLAGS=\"-L$HDF5_DIR/lib -lhdf5 -lhdf5_hl\"  >> compilation_vars.txt
 echo export PATH=\"/c/Python37:/c/Python37/Scripts:$PATH\" >> compilation_vars.txt
