8a45cc35581527d14ce83ee8c35843fd0312719f
Joel Rahman <joel@flowmatters.com.au>
2020-11-07 12:14:52 +1100

    Add delivery ratio model (another simple scaling model separated for convenience)

---
+++ models/conversion/delivery_ratio.go
@@ -0,0 +1,22 @@
+package conversion
+
+/*OW-SPEC
+DeliveryRatio:
+  inputs:
+		input:
+	states:
+	parameters:
+		fraction: 'default=1'
+	outputs:
+		output:
+	implementation:
+		function: applyScaling
+		type: scalar
+		lang: go
+		outputs: params
+	init:
+		zero: true
+		lang: go
+	tags:
+		partition
+*/
