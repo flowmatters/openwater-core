b7bf7223c42be4521d300692ea6b0660f75dd262
Joel Rahman <joel@flowmatters.com.au>
2021-04-22 15:49:43 +1000

    HDF5 utils

--- build/ci/linux.sh
+++ build/ci/linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python3 libhdf5-serial-dev python3-venv
-
+sudo apt-get install -y python3 libhdf5-serial-dev hdf5-tools python3-venv
 
 
 h5ls --version
