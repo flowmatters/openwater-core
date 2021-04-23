691d28fa913fd55b78fa6c3d5bd547790c4783e3
Joel Rahman <joel@flowmatters.com.au>
2021-04-23 11:12:41 +1000

    particulate trapping model for storages

---
+++ models/storage/sediment_trapping.go
@@ -0,0 +1,73 @@
+package storage
+
+import (
+	"math"
+
+	"github.com/flowmatters/openwater-core/data"
+	"github.com/flowmatters/openwater-core/util/m"
+)
+
+/* OW-SPEC
+StorageParticulateTrapping:
+	inputs:
+		inflowLoad: kg.s^-1
+		inflow: m^3.s^-1
+		outflow: m^3.s^-1
+		storage: m^3
+	states:
+		storedMass: kg
+	parameters:
+		DeltaT: '[1,86400] Timestep, default=86400'
+		reservoirCapacity:
+		reservoirLength: 'Length (m) of reservoir from dam wall to longest impounded water at dam capacity'
+		subtractor: 'default=112'
+		multiplier: 'default=800'
+		lengthDischargeFactor:
+		lengthDischargePower:
+	outputs:
+		trappedMass: kg
+		outflowMass: kg.s^-1
+	implementation:
+		function: storageParticulateTrapping
+		type: scalar
+		lang: go
+		outputs: params
+	init:
+		zero: true
+		lang: go
+	tags:
+		storage, sediment
+*/
+
+func storageParticulateTrapping(inflowMass, storageInflow, storageOutflow, storageVolume data.ND1Float64, // inputs
+	initialStoredMass float64,
+	deltaT, reservoirCapacity, reservoirLength, subtractor, multiplier, lengthDischargeFactor, lengthDischargePower float64,
+	trappedMass, outflowMass data.ND1Float64) (storedMass float64) {
+	storedMass = initialStoredMass
+	n := inflowMass.Len1()
+	idx := []int{0}
+	for i := 0; i < n; i++ {
+		idx[0] = i
+		incomingMass := inflowMass.Get(idx) * deltaT
+		inflowRate := storageInflow.Get(idx)
+
+		damTrappingPC := 0.0
+
+		if (inflowRate > 0) && (reservoirLength > 0) {
+			sedimentationIndex := math.Pow(reservoirCapacity, 2.0) / (lengthDischargeFactor * reservoirLength * math.Pow(inflowRate, 2.0))
+			damTrappingPC = subtractor - (multiplier * math.Pow(sedimentationIndex, lengthDischargePower))
+			damTrappingPC = m.MinFloat64(100.0, m.MaxFloat64(0.0, damTrappingPC))
+		}
+
+		dailyTrappedConstituentLoad := incomingMass * damTrappingPC / 100.0
+		trappedMass.Set(idx, dailyTrappedConstituentLoad)
+
+		storedMass = storedMass + incomingMass - dailyTrappedConstituentLoad
+
+		concentration := storedMass / storageVolume.Get(idx) // kg/m^3
+		massOutRate := storageOutflow.Get(idx) * concentration
+		outflowMass.Set(idx, massOutRate)
+	}
+
+	return
+}
