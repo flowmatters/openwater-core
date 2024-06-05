b62269766ac589b1f4074babb6f239db3e87d35f
Joel Rahman <joel@flowmatters.com.au>
2024-06-05 16:39:11 +1000

    added to path

--- build/ci/osx.sh
+++ build/ci/osx.sh
@@ -6,7 +6,10 @@ brew link hdf5
 brew install go
 echo "######################"
 ls -a /opt/homebrew/include | grep hdf5
+ls -a /opt/homebrew/lib | grep hdf5
+uname -a
 export PATH=$PATH:/opt/homebrew/include
+export PATH=$PATH:/opt/homebrew/lib
 # brew install tree
 # tree /
 # find / -name "hdf5.h"
