3dcfe360ded8a9ed87bc523039b46b1bfbbce32e
Joel Rahman <joel@flowmatters.com.au>
2024-06-05 16:52:02 +1000

    downgraded hdf5

--- build/ci/osx.sh
+++ build/ci/osx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 brew update
-brew install hdf5
-brew link hdf5
 brew install go
+brew install hdf5@1.8
+brew link hdf5
 echo "######################"
 ls -a /opt/homebrew/include | grep hdf5
 ls -a /opt/homebrew/lib | grep hdf5
@@ -15,6 +15,7 @@ export PATH=$PATH:/opt/homebrew/lib
 # find / -name "hdf5.h"
 export CGO_FLAGS="-I/opt/homebrew/include"
 export CFLAGS="-I/opt/homebrew/include/"
+export CPPFLAGS="-I/opt/homebrew/include/"
 export LDFLAGS="-L/opt/homebrew/lib"
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/homebrew/include
 printenv CGO_FLAGS
