7f5404cde638a6d7aab58ea9b032440f9f671af2
Joel Rahman <joel@flowmatters.com.au>
2024-06-05 16:29:32 +1000

    osx build hdf5

--- build/ci/osx.sh
+++ build/ci/osx.sh
@@ -2,6 +2,7 @@
 
 brew update
 brew install hdf5
+brew link hdf5
 brew install go
 echo "######################"
 ls -a /opt/homebrew/include | grep hdf5
@@ -10,5 +11,7 @@ export PATH=$PATH:/opt/homebrew/include
 # tree /
 # find / -name "hdf5.h"
 export CGO_FLAGS="-I/opt/homebrew/include"
+export CFLAGS="-I/opt/homebrew/include/"
+export LDFLAGS="-L/opt/homebrew/lib"
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/homebrew/include
 printenv CGO_FLAGS
