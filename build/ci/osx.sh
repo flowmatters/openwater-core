e2d2071521c41d3eba0e6020c418409dd0adb265
Joel Rahman <joel@flowmatters.com.au>
2024-06-05 16:04:38 +1000

    find hdf5.h osx

--- build/ci/osx.sh
+++ build/ci/osx.sh
@@ -4,8 +4,9 @@ brew update
 brew install hdf5
 brew install go
 echo "######################"
-ls -a /usr/local/include
-brew install tree
-tree /
-find / -name "hdf5.h"
-export CPATH="/usr/include/"
+ls -a /opt/homebrew/include
+export PATH=$PATH:/opt/homebrew/include
+# brew install tree
+# tree /
+# find / -name "hdf5.h"
+export CPATH="/opt/homebrew/include/"
