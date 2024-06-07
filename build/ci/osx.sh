9e1bee4f1e64c9584ce5a93dda872678e8a66922
Joel Rahman <joel@flowmatters.com.au>
2024-06-07 12:13:25 +1000

    upload artifacts osx

--- build/ci/osx.sh
+++ build/ci/osx.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 brew update
-brew install hdf5
-brew link hdf5
 brew install go
-echo "######################"
-ls -a /opt/homebrew/include | grep hdf5
-export PATH=$PATH:/opt/homebrew/include
+brew install hdf5
+# brew link hdf5
+# echo "######################"
+# sudo cp /opt/homebrew/include/* /usr/local/include/
+# sudo cp /opt/homebrew/lib/* /usr/local/lib/
+# arch=$(uname -i)
+# if [[$arch == arm*]]; then
+# sudo mkdir -p /usr/local
+sudo ln -s /opt/homebrew/include /usr/local/include
+sudo ln -s /opt/homebrew/lib /usr/local/lib
+# echo "usr"
+# ls -a /usr/local/include | grep hdf5
+# ls -a /usr/local/lib | grep hdf5
+# echo "homebrew"
+# ls -a /opt/homebrew/include | grep hdf5
+# ls -a /opt/homebrew/lib | grep hdf5
+# fi
+# uname -a
+# export PATH=$PATH:/opt/homebrew/include
+# export PATH=$PATH:/opt/homebrew/lib
 # brew install tree
 # tree /
 # find / -name "hdf5.h"
-export CGO_FLAGS="-I/opt/homebrew/include"
-export CFLAGS="-I/opt/homebrew/include/"
-export LDFLAGS="-L/opt/homebrew/lib"
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/homebrew/include
-printenv CGO_FLAGS
+# export CGO_CFLAGS="-I/opt/homebrew/include"
+# export CGO_LDFLAGS="-L/opt/homebrew/lib -lhdf5 -lhdf5_hl"
+# export CFLAGS="-I/opt/homebrew/include/"
+# export CPPFLAGS="-I/opt/homebrew/include/"
+# export LDFLAGS="-L/opt/homebrew/lib"
+# export C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/homebrew/include
+# printenv CGO_FLAGS
