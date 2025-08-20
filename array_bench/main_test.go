7ad1a9117427f3297409614e362f158470480dff
David Rahman <david@flowmatters.com.au>
2025-08-20 12:00:42 +1000

    array benchmarking scripts

--- array_bench/main_test.go
+++ array_bench/main_test.go
@@ -2,6 +2,7 @@ package arraybench
 
 import (
 	// "runtime/pprof",
+	"iter"
 	"testing"
 
 	"github.com/flowmatters/openwater-core/data"
@@ -70,6 +71,85 @@ const ARRAY_SIZE = SUB_ARRAY_COUNT * SUB_ARRAY_SIZE
 // 	}
 // }
 
+func BenchmarkND3AsSliceIterGetPull_run(b *testing.B) {
+	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
+	if !as.Contiguous() {
+		b.Fatalf("Array is not contiguous")
+	}
+	b.ResetTimer()
+	idx := []int{0}
+	for b.Loop() {
+		values := as.Values(idx, 0, ARRAY_SIZE)
+		next, _ := iter.Pull(values)
+		// for v := range values {
+		for j := 0; j < ARRAY_SIZE; j++ {
+			v, _ := next()
+			as.Set(idx, v+1)
+		}
+		// }
+	}
+}
+
+func BenchmarkND3AsSliceIterGetRange_run(b *testing.B) {
+	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
+	if !as.Contiguous() {
+		b.Fatalf("Array is not contiguous")
+	}
+	b.ResetTimer()
+	idx := []int{0}
+	for b.Loop() {
+		values := as.Values(idx, 0, ARRAY_SIZE)
+		// for v := range values {
+		for v := range values {
+			as.Set(idx, v+1)
+		}
+		// }
+	}
+}
+
+func BenchmarkND3AsSliceRawIterGet_run(b *testing.B) {
+	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
+	if !as.Contiguous() {
+		b.Fatalf("Array is not contiguous")
+	}
+	b.ResetTimer()
+	idx := []int{0}
+	for b.Loop() {
+		for v := range as.Unroll() {
+			as.Set(idx, v+1)
+		}
+	}
+}
+
+func BenchmarkFullRawIO_run(b *testing.B) {
+	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
+	if !as.Contiguous() {
+		b.Fatalf("Array is not contiguous")
+	}
+	b.ResetTimer()
+	idx := []int{0}
+	for b.Loop() {
+		index := as.RawIndices(idx, 0, ARRAY_SIZE)
+		for i := range index {
+			as.SetRaw(i, as.GetRaw(i)+1)
+		}
+	}
+}
+
+func BenchmarkFullRawIONoIter_run(b *testing.B) {
+	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
+	if !as.Contiguous() {
+		b.Fatalf("Array is not contiguous")
+	}
+	b.ResetTimer()
+	for b.Loop() {
+		offset := as.OffsetStep[0]
+		for i := 0; i < ARRAY_SIZE; i += offset {
+			as.SetRaw(i, as.GetRaw(i)+1)
+		}
+	}
+}
+
 func BenchmarkND3AsSlice_run(b *testing.B) {
 	as := data.NewArray3D[int](300, 400, ARRAY_SIZE).Slice([]int{0, 0, 0}, []int{1, 1, ARRAY_SIZE}, nil).MustReshape([]int{ARRAY_SIZE})
 	if !as.Contiguous() {
@@ -86,47 +166,47 @@ func BenchmarkND3AsSlice_run(b *testing.B) {
 	}
 }
 
-func makeArraySlice[T data.Number]() []([SUB_ARRAY_SIZE]T) {
-	return make([][SUB_ARRAY_SIZE]T, SUB_ARRAY_COUNT)
-}
+// func makeArraySlice[T data.Number]() []([SUB_ARRAY_SIZE]T) {
+// 	return make([][SUB_ARRAY_SIZE]T, SUB_ARRAY_COUNT)
+// }
 
-func BenchmarkArraySliceIterator_run(b *testing.B) {
-	as := makeArraySlice[int]()
-	b.ResetTimer()
-	for b.Loop() {
-		for k := range as {
-			for j := range as[k] {
-				as[k][j] += 1
-			}
-		}
-		// as = makeArraySlice[int]()
-	}
-}
+// func BenchmarkArraySliceIterator_run(b *testing.B) {
+// 	as := makeArraySlice[int]()
+// 	b.ResetTimer()
+// 	for b.Loop() {
+// 		for k := range as {
+// 			for j := range as[k] {
+// 				as[k][j] += 1
+// 			}
+// 		}
+// 		// as = makeArraySlice[int]()
+// 	}
+// }
 
 // From https://go-benchmarks.com/array-vs-slice
 // BenchmarkArray_run seems ridiculously fast?
-func BenchmarkArray_run(b *testing.B) {
-	var arr [ARRAY_SIZE]int // Define an array of fixed size
+// func BenchmarkArray_run(b *testing.B) {
+// 	var arr [ARRAY_SIZE]int // Define an array of fixed size
 
-	b.ResetTimer()
+// 	b.ResetTimer()
 
-	for b.Loop() {
-		for j := range arr {
-			arr[j] += 1
-		}
-		// arr = [ARRAY_SIZE]int{} // Reset the array
-	}
-}
+// 	for b.Loop() {
+// 		for j := range arr {
+// 			arr[j] += 1
+// 		}
+// 		// arr = [ARRAY_SIZE]int{} // Reset the array
+// 	}
+// }
 
-func BenchmarkPreallocatedSlice_run(b *testing.B) {
-	slice := make([]int, ARRAY_SIZE) // Define a slice with the same size as the array
+// func BenchmarkPreallocatedSlice_run(b *testing.B) {
+// 	slice := make([]int, ARRAY_SIZE) // Define a slice with the same size as the array
 
-	b.ResetTimer()
+// 	b.ResetTimer()
 
-	for b.Loop() {
-		for j := range slice {
-			slice[j] += 1
-		}
-		// slice = make([]int, ARRAY_SIZE) // Reset the slice
-	}
-}
+// 	for b.Loop() {
+// 		for j := range slice {
+// 			slice[j] += 1
+// 		}
+// 		// slice = make([]int, ARRAY_SIZE) // Reset the slice
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/flowmatters/openwater-core
 
-go 1.18
+go 1.23
 
 require (
 	github.com/golang/protobuf v1.4.3
